96945a86ee3b651904dc20924e23d59edda2771e
Carlos de Paula <me@carlosedp.com>
2023-10-17 20:18:42 -0300

    Cleanup

--- backend/src/HomeApp.scala
+++ backend/src/HomeApp.scala
@@ -4,9 +4,9 @@ package zioscalajs.backend
 import zio.*
 import zio.http.*
 
-// Just redirect requests from "/" to "/greet"
 object HomeApp:
   def apply(): Http[Any, Nothing, Request, Response] =
     Http.collectZIO[Request]:
+      // GET /, redirect to /greet )
       case Method.GET -> Root =>
-        ZIO.succeed(Response.redirect(URL(Root / "greet"))) // GET /, redirect to /greet )
+        ZIO.succeed(Response.redirect(URL(Root / "greet")))
